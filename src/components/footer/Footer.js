b1e13cce4a49c3e128028371f671de0f3559a295
JuliaVyl <julia.vyl@gmail.com>
2020-11-27 19:32:15 +0300

    Make Footer component
    Write html and css code

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,6 +1,20 @@
+import './footer.css';
+
 const Footer = () => {
   return (
-    <div className="footer"></div>
+    <div className="footer">
+      <div className="footer__wrapper">
+          <div className="footer__logo">Bookstore</div>
+          <div className="footer__email">
+            <p className="footer__email-text">Charity your old books</p>
+            <a className="footer__email-link" href="mailto:julia.vyl@gmail.com">Click here</a>
+          </div>
+          <div className="footer__contacts">
+            <a className="footer__contacts-links" href="#!">github</a>
+            <a className="footer__contacts-links" href="https://www.instagram.com/julvyl/">instagram</a>
+          </div>
+      </div>
+    </div>
   )
 }
 
